5733f7381fede009d6a40af39235ed44e6a43026
teundeplanque <teun@stanford.edu>
2014-08-10 22:55:26 -0700

    Create xGlove_Gesture.h

---
+++ xGlove_Gesture.h
@@ -0,0 +1,25 @@
+/* xGlove_Gesture.h
+ * ----------------
+ * still under construction
+ * 
+ */
+#ifndef xGlove_Gesture_h
+#define xGlove_Gesture_h
+
+#include "Arduino.h"
+#include "xGlove_Sensor.h"
+
+class Gesture
+{
+  
+    public:
+    Gesture();
+    boolean phist();
+    
+    
+    private:
+  
+};
+
+
+#endif
