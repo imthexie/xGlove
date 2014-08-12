854abfcc43db7fbd6eb2133d96dd652e6c06302b
teundeplanque <teun@stanford.edu>
2014-08-11 23:51:43 -0700

    Update xGlove_Gesture.h

--- xGlove_Gesture.h
+++ xGlove_Gesture.h
@@ -9,17 +9,13 @@
 #include "Arduino.h"
 #include "xGlove_Sensor.h"
 
-class Gesture
-{
-  
-    public:
-    Gesture();
-    boolean phist();
-    
-    
-    private:
-  
-};
 
+    boolean fist();
+    boolean fingers_spread();
+    boolean upside_down();
+    boolean right_side_up()
+    
+
+ 
 
 #endif
