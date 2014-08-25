202740c315a5187cd789771cc5f67e0140e97bc7
imthexie <sxie@stanford.edu>
2014-08-25 13:23:00 -0700

    Fix braces

--- xGlove/src/xGloveGesture.java
+++ xGlove/src/xGloveGesture.java
@@ -1,38 +1,46 @@
-class xGloveGesture {
+class xGloveGesture 
+{
     private xGloveSensor sensor; //reference to the sensor
 
     int minima[] = {0, -40};         // actual analogRead minima for {x, y}
     int maxima[] = {0, 40};          // actual analogRead maxima for {x, y}
 
-    public xGloveGesture() {
+    public xGloveGesture() 
+    {
         this.sensor = xGloveDispatcher.getSensor();
     }
 
-    public Orientation getOrientation() {
+    public Orientation getOrientation() 
+    {
         return sensor.getOrientation();
     }
 
-    public boolean fist() {
+    public boolean fist() 
+    {
         return sensor.allFingersBent(65);
     }
 
-    public boolean fingersBent() {
+    public boolean fingersBent() 
+    {
         return sensor.allFingersBent(50);
     }
 
-    public boolean fingersSpread() {
+    public boolean fingersSpread() 
+    {
         return !sensor.allFingersBent(50);
     }
 
 
     //TODO: implement this with a Timer instead. Or just make this event a non-delayed event
-    public boolean upsideDown(){
+    public boolean upsideDown()
+    {
         if(Math.abs(sensor.getOrientation().roll) > 170)
         {
             /* This for loop is used to ensure that the glove is upside down */
             /* for at least .5 second (10*delay(50)).                        */
             /* This is to avoid measurement errors.                          */         
-            for(int i = 0; i < 10; i+=1){
+            for(int i = 0; i < 10; i+=1)
+            {
             	//break if sensor is back in original position
                 if(Math.abs(sensor.getOrientation().roll) < 115) return false;
 
@@ -45,40 +53,49 @@ class xGloveGesture {
     }
 
     //TODO: gesture for spacebar
-    public boolean isSpacebarGesture() {
+    public boolean isSpacebarGesture() 
+    {
         //??
         return false;   
     }
 
-    public boolean rightSideUp(){
+    public boolean rightSideUp()
+    {
         return Math.abs(sensor.getOrientation().roll) < 50;
     }  
 
-    public boolean isMouseClickGesture(boolean currentlyClicked) {
+    public boolean isMouseClickGesture(boolean currentlyClicked) 
+    {
         return (fist() && !currentlyClicked && Math.abs(sensor.getOrientation().pitch) < 35);
     }
 
-    public boolean isMouseReleaseGesture(boolean currentlyClicked) {
+    public boolean isMouseReleaseGesture(boolean currentlyClicked) 
+    {
     	return currentlyClicked && sensor.allFingersSpread();
     }
 
-    public boolean isScrollModeGesture() {
+    public boolean isScrollModeGesture() 
+    {
         return sensor.ringFingerBent(90) && !(sensor.middleFingerBent(50)) && !(sensor.indexFingerBent(50));
     }
 
-    public boolean isReleaseScrollModeGesture() {
+    public boolean isReleaseScrollModeGesture() 
+    {
         return sensor.ringFingerBent(70) && !(sensor.middleFingerBent(50));
     }
 
-    public boolean isLoadNextGesture() {
+    public boolean isLoadNextGesture() 
+    {
         return sensor.getOrientation().pitch > 70;
     }
 
-    public boolean isLoadPreviousGesture() {
+    public boolean isLoadPreviousGesture() 
+    {
         return sensor.getOrientation().pitch < -70;
     }
 
-    public int getInclinationPercentage() {
+    public int getInclinationPercentage() 
+    {
         return sensor.getInclinationPercentage();
     }
 }
